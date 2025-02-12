9ed980425dcdbe8b62206ff33206685e0b9f3a0b
Seyit <knowwhoscoming@gmail.com>
2025-02-12 01:56:15 +0100

    Update deployment configuration and debugging

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [react()],
   server: {
     port: 3002,
-    host: true
   }
-}) 
+})
