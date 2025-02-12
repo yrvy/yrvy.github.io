fea54efa5cccba5ca461b981a35ce4793d71128f
Seyit <knowwhoscoming@gmail.com>
2025-02-12 02:57:58 +0100

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-  server: {
-    port: 3002,
-    host: true
+  base: '/yrvy.github.io/',
+  build: {
+    outDir: 'dist'
   }
-}) 
+})
